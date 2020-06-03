ce235607ce06ee6beafc928c94930b8e2731a5de
paisonthebaison <59052989+paisonthebaison@users.noreply.github.com>
2020-06-03 15:35:55 +0900

    kaminari-and-render-fin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,6 @@ class UsersController < ApplicationController
     user = User.find(params[:id])
     @nickname = user.nickname
     @tweets = user.tweets
+    @tweets = user.tweets.page(params[:page]).per(5).order("created_at DESC")
   end
 end
